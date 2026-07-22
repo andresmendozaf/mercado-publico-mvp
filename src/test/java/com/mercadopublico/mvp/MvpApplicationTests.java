d588e7ba0b35a1f9f3be25624fd19e6a7879a75a
Andrés Mendoza <El Bosque>
2026-07-21 20:08:49 -0400

    fix: configurar propiedades dummy en MvpApplicationTests para CI/CD

--- src/test/java/com/mercadopublico/mvp/MvpApplicationTests.java
+++ src/test/java/com/mercadopublico/mvp/MvpApplicationTests.java
@@ -3,8 +3,10 @@ package com.mercadopublico.mvp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MvpApplicationTests {
+@SpringBootTest(properties = {
+    "MERCADO_PUBLICO_TICKET=test-ticket-dummy",
+    "spring.profiles.active=test"
+})class MvpApplicationTests {
 
 	@Test
 	void contextLoads() {
